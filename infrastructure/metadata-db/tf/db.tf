665996183ec5b2016f1247a3d88e04b18cb04949
Roger Duthie <343584+rjaduthie@users.noreply.github.com>
2026-01-21 12:32:16 +0000

    fix(SOG-396): Ensure .env with db passwords is loaded before Ansible run

--- infrastructure/metadata-db/tf/db.tf
+++ infrastructure/metadata-db/tf/db.tf
@@ -1,3 +1,17 @@
+locals {
+  ansible_command = <<-EOT
+		sleep 60
+    cd ${path.module}/../ansible
+		set -a
+    [ -f .env ] && source .env
+    set +a
+    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+      -i '${openstack_compute_instance_v2.postgres_vm.access_ip_v4},' \
+      -u ${local.primary_user} \
+      postgres.yml
+	EOT
+}
+
 resource "null_resource" "postgres_provisioner" {
   depends_on = [openstack_compute_instance_v2.postgres_vm]
 
@@ -5,16 +19,12 @@ resource "null_resource" "postgres_provisioner" {
     instance_id = openstack_compute_instance_v2.postgres_vm.id
     # Re-run if playbook changes
     playbook_hash = filemd5("${path.module}/../ansible/postgres.yml")
+    # Re-run if provisioner command changes
+    provisioner_command = md5(local.ansible_command)
   }
 
   provisioner "local-exec" {
-    command = <<-EOT
-            sleep 60
-            cd ${path.module}/../ansible
-            ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
-              -i '${openstack_compute_instance_v2.postgres_vm.access_ip_v4},' \
-              -u ${local.primary_user} \
-              postgres.yml
-        EOT
+		interpreter = ["/bin/bash", "-c"]
+    command = local.ansible_command
   }
 }
