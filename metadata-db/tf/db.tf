68f5cf40ad81dfa1d1a69290017f8ed1c40f1e82
Roger Duthie <343584+rjaduthie@users.noreply.github.com>
2026-01-29 12:05:18 +0000

    feat(SOG-453): Add SDS schema to database host

--- metadata-db/tf/db.tf
+++ metadata-db/tf/db.tf
@@ -11,7 +11,8 @@ locals {
 
   system_update_command       = "sleep 60\n${local.ansible_base_command} update_system.yml"
   postgres_install_command    = "${local.ansible_base_command} install_and_configure_postgres.yml"
-  caomdb_schema_setup_command = "${local.ansible_base_command} setup_metadata_db_schema.yml"
+  caomdb_schema_setup_command = "${local.ansible_base_command} setup_mm_db_schema.yml"
+  sdsdb_schema_setup_command  = "${local.ansible_base_command} setup_sds_db_schema.yml"
 }
 
 resource "null_resource" "system_update_provisioner" {
@@ -54,12 +55,15 @@ resource "null_resource" "postgres_install_provisioner" {
   }
 }
 
+
+// Schemas
+
 resource "null_resource" "caomdb_schema_provisioner" {
   depends_on = [null_resource.postgres_install_provisioner]
 
   triggers = {
     host_instance_id = openstack_compute_instance_v2.postgres_vm.id
-    playbook_hash = filemd5("${path.module}/../ansible/setup_metadata_db_schema.yml")
+    playbook_hash = filemd5("${path.module}/../ansible/setup_mm_db_schema.yml")
     vars_hash = md5(join("", [
       filemd5("${path.module}/../ansible/group_vars/all/schema/caomdb.yml"),
       filemd5("${path.module}/../ansible/group_vars/all/postgres.yml"),
@@ -72,5 +76,26 @@ resource "null_resource" "caomdb_schema_provisioner" {
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = local.caomdb_schema_setup_command
+  }
+}
+
+resource "null_resource" "sds_schema_provisioner" {
+  depends_on = [null_resource.postgres_install_provisioner]
+
+  triggers = {
+    host_instance_id = openstack_compute_instance_v2.postgres_vm.id
+    playbook_hash = filemd5("${path.module}/../ansible/setup_metadata_db_schema.yml")
+    vars_hash = md5(join("", [
+      filemd5("${path.module}/../ansible/group_vars/all/schema/sdsdb.yml"),
+      filemd5("${path.module}/../ansible/group_vars/all/postgres.yml"),
+      try(filemd5("${path.module}/../ansible/.env"), "")
+    ]))
+    role_hash = md5(join("", [for f in fileset("${path.module}/../ansible/roles/setup_metadata_db_schema", "**") : filemd5("${path.module}/../ansible/roles/setup_metadata_db_schema/${f}")]))
+    provisioner_command_hash = md5(local.sdsdb_schema_setup_command)
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = local.sdsdb_schema_setup_command
   }
 }
