e063a765f64444296da37a9560bc1e432e3b8039
Simon Bowden <sb2882@cam.ac.uk>
2026-04-27 13:18:45 +0000

    fix(DAAC-853): switch the cert copy script to a template and make the server name a variable

--- xrootd/ansible/roles/cert_setup/files/copy-xrootd-certs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp /etc/letsencrypt/live/xrootd01.cam.uksrc.org/fullchain.pem /etc/grid-security/xrootd/hostcert.pem
-cp /etc/letsencrypt/live/xrootd01.cam.uksrc.org/privkey.pem /etc/grid-security/xrootd/hostkey.pem
-systemctl restart xrootd@config
