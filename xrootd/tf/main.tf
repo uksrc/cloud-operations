e0e2ecd25a3891652aedfa6446779678e3a035e8
Mansoor Ali <ma2223@cam.ac.uk>
2026-07-31 12:35:04 +0100

    POG-83: Fixing binding error issue

--- xrootd/tf/main.tf
+++ xrootd/tf/main.tf
@@ -86,13 +86,9 @@ resource "openstack_networking_port_v2" "ska_uksrc_wcdc_dirac_sriov_port" {
   admin_state_up = "true"
   port_security_enabled = false
 
-  value_specs = {
-    "binding:vnic_type" = "direct"
-
-    # Optional: trusted VF
-    "binding:profile" = jsonencode({
-      trusted = true
-    })
+  binding {
+    vnic_type = "direct"
+    profile   = jsonencode({ trusted = true })
   }
 
   fixed_ip {
